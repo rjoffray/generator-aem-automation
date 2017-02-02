567b752d44c9f6bcb85b3d6c38c06ff0d73a029d
rjoffray <richard@joffray.com>
2017-02-02 10:12:47 -0600

    Update to index

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,8 +5,11 @@ var yosay = require('yosay');
 var path = require('path');
 var dsj = require('directory-structure-json');
 var fs = require('fs');
-var baseTemplatePath = __dirname + "/templates";
-
+var baseTemplatePath = __dirname + "/templates-real";
+var baseDestinationPath = __dirname + "/dist";
+var baseStructure = [];
+var find = '\<%= siteName %\>';
+var re = new RegExp(find, 'g');
 module.exports = Generator.extend({
   prompting: function () {
     // Have Yeoman greet the user.
@@ -15,10 +18,8 @@ module.exports = Generator.extend({
     ));
 
     var prompts = [{
-      type: 'confirm',
       name: 'siteName',
-      message: 'Would you like to name your site?',
-      default: true
+      message: 'Would you like to name your site?'
     }];
 
     return this.prompt(prompts).then(function (props) {
@@ -28,13 +29,43 @@ module.exports = Generator.extend({
   },
 
   writing: function () {
-    console.log("baseTemplatePath: " + baseTemplatePath)
-    dsj.getStructure(fs, baseTemplatePath, function (err, structure, total) {
-      if (err) console.log(err);
+    console.log("templatePath: " + this.templatePath());
+      console.log("destinationPath: " + baseDestinationPath);
+      var _this = this;
 
-      console.log('there are a total of: ', total.folders, ' folders and ', total.files, ' files');
-      // console.log('the structure looks like: ', JSON.stringify(structure, null, 4));
-    });
+    //_this.fs.copyTpl(baseTemplatePath,baseDestinationPath,_this.props);
+        dsj.getStructure(fs, _this.templatePath(), function (err, structure, total) {
+            if (err) console.log(err);
+            //console.log("You said: " + _this.props.siteName)
+
+            dsj.traverseStructure(structure, _this.templatePath(),
+                function (folder, path) {
+                    var newFolderName = folder.name.replace(re,_this.props.siteName);
+                    var newPath = path.substring(_this.templatePath().length).replace(re,_this.props.siteName);
+                    var relativeFolderPath = baseDestinationPath + newPath + "/" + newFolderName;
+                    console.log("relativeFolderPath: " + relativeFolderPath)
+                    if (!fs.existsSync(relativeFolderPath)){
+                        //fs.mkdirSync(relativeFolderPath);
+                    }
+                },
+                function (file, path) {
+                    var fileTemplatePath = path + "/" + file.name;
+                    //console.log("fileTemplatePath: " + path + "/" + file.name)
+                    var newFileName  = file.name.replace(re,_this.props.siteName);
+                    var relativeFilePath = baseDestinationPath  + path.substring(_this.templatePath().length).replace(re,_this.props.siteName) + "/" + newFileName;
+                    console.log("relativeFilePath: " + relativeFilePath)
+                    if(file.name.startsWith("__")){
+                        //_this.fs.copyTpl(fileTemplatePath, relativeFilePath, _this.props);
+                    }else{
+                        //_this.fs.copy(fileTemplatePath, relativeFilePath);
+                    }
+
+                });
+            console.log('there are a total of: ', total.folders, ' folders and ', total.files, ' files');
+            //console.log('the structure looks like: ', JSON.stringify(structure, null, 4));
+        });
+
+
   },
 
   install: function () {
